695918829d21bbb63190e8716bcae7dffe243d98
Atul <49794348+atul2909@users.noreply.github.com>
2026-05-11 12:30:17 -0400

    Update Card.jsx
    udpated cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -9,17 +9,17 @@ const Card = () => {
     <div className="card">
       <div className="cardImg">
         <img src={Dine} alt="dineImg" />
-        <h1>Order Online</h1>
+        <h1>Order Online and Study Terraform</h1>
         <span>Stay home and order to your doorstep</span>
       </div>
       <div className="cardImg">
         <img src={Night} alt="nightImg" />
-        <h1>Nightlife and Clubs</h1>
+        <h1>Order Online and Study Ansible</h1>
         <span>Explore the city's top nightlife outlets</span>
       </div>
       <div className="cardImg">
         <img src={Online} alt="onlineImg" />
-        <h1>Dinning</h1>
+        <h1>Order Online and get Trained with Raham</h1>
         <span>Views the city's favourite venues</span>
       </div>
     </div>
